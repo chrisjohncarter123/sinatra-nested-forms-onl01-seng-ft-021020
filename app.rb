77dee047f3394238f0ed31ed4e71a5188a65cbfc
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-25 00:28:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,28 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      erb :'pirates/new'
+    end
+    
+    post '/pirates' do
+      
+      @pirate = Pirate.new(params[:pirate])
+ 
+      params[:ships].each do |details|
+        Ship.new(details)
+      end
+     
+      @ships = Ships.all
+      
+      erb :'pirates/show'
 
+    end
   end
+    
 end
